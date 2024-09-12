db8f4971392bdb31517e172ce47c64dbd00f81ad
root <root@localhost.localdomain>
2024-09-12 12:12:33 -0500

    Rotate debug log on every start.

--- start.sh
+++ start.sh
@@ -2,4 +2,11 @@
 
 [[ -z "${POCKETCONF}" ]] && POCKETCONF="$HOME/.pocketcoin/pocketcoin.conf" || POCKETCONF="${POCKETCONF}"
 
-pocketcoind -conf="${POCKETCONF}" -daemon
+echo "Using configuration file: $POCKETCONF"
+
+SCRIPT_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
+ROTATE_PATH="$SCRIPT_PATH/rotate_debuglog.sh"
+
+source $ROTATE_PATH
+
+#pocketcoind -conf="${POCKETCONF}" -daemon
